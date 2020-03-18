348b86b6b7f12bab09fd13096f0e13a9c9adfd23
WMXPY <wm@mengw.io>
2020-03-18 16:32:38 -0500

    add minor fixes, deploy 1.0.1

--- src/section/implement/header.tsx
+++ src/section/implement/header.tsx
@@ -7,8 +7,12 @@
 import * as React from "react";
 import { LandingTheme } from "../../theme/declare";
 import { ThemeProps, withTheme } from "../../theme/theme";
+import { Classes, mergeClasses, assertIfTrue } from "@sudoo/jss";
+import { HeaderStyle } from "../style/header.style";
 
 export type HeaderProps = {
+
+    readonly sticky?: boolean;
 
     readonly style?: React.CSSProperties;
 };
@@ -17,15 +21,34 @@ type HeaderWithThemeProps = HeaderProps & ThemeProps;
 
 class HeaderBase extends React.PureComponent<HeaderWithThemeProps> {
 
+    private readonly _headerStyle: Classes = HeaderStyle.use();
+
     public render() {
+
+        return (<header
+            className={mergeClasses(
+                assertIfTrue(this.props.sticky, this._headerStyle.sticky),
+            )}
+            style={{
+                ...this._getHeaderStyle(),
+                ...this.props.style,
+            }}
+        >
+            <div>{this.props.children}</div>
+        </header>);
+    }
+
+    private _getHeaderStyle(): React.CSSProperties {
 
         const theme: LandingTheme = this.props.theme;
 
-        return (<header style={{
-            ...this.props.style,
-        }}>
-            <div>{this.props.children}</div>
-        </header>);
+        if (!this.props.sticky) {
+            return {};
+        }
+
+        return {
+            backgroundColor: theme.color.majorColor.background,
+        };
     }
 }
 
--- src/section/style/header.style.ts
+++ src/section/style/header.style.ts
@@ -8,6 +8,12 @@ import { StyleManager, Styles } from "@sudoo/jss";
 import { landingStyleCollection } from "../../style";
 
 export const HeaderStyleBase: Styles = {
+
+    sticky: {
+        position: 'sticky',
+        left: 0,
+        top: 0,
+    },
 };
 
 export const HeaderStyle: StyleManager = landingStyleCollection.hydrate('Section-Header', HeaderStyleBase);
